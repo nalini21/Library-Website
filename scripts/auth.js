174c3f23efc676f987f7614ea659e38532b4ee95
nalini21 <naliniranjan21@gmail.com>
2020-07-01 23:50:13 +0530

    content added, email verification can be send

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,28 +1,39 @@
-db.collection('books').get().then(snapshot =>{
-    setupBooks(snapshot.docs);
-});
 
 //listen for auth changes
-auth.onAuthStateChanged(user=>{
-    if(user){
-        console.log('user is logged in');
-        console.log(user);    
+auth.onAuthStateChanged(user => {
+    if (user) {
+        //if (user.emailVerified) {
+            console.log(user);
+            db.collection('books').get().then(snapshot => {
+                setupBooks(snapshot.docs);
+            });
+            setupUI(user);
+            setupAccount(user);
+        //}
+        // else{
+        //     setupUI();
+        //     setupBooks([]);
+        //     window.alert('verify your email to see the content');
+        //     console.log(user);    
+        // }
     }
-    else{
-        console.log('user is logged out');
+    else {
+        setupUI();
+        setupBooks([]);
+        console.log('user is logged out');       
     }
-    
 });
 
 const signupForm = document.querySelector('#signup-form');
-signupForm.addEventListener('submit',(evt)=>{
+signupForm.addEventListener('submit', (evt) => {
 
     evt.preventDefault();
     //user info
     const email = signupForm['signup-email'].value;
     const password = signupForm['signup-password'].value;
     //sign up user
-    auth.createUserWithEmailAndPassword(email, password).then(cred =>{
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        send_verification();
         //close the modal
         const modal = document.querySelector('#modal-signup');
         M.Modal.getInstance(modal).close();
@@ -31,21 +42,53 @@ signupForm.addEventListener('submit',(evt)=>{
 });
 
 const logout = document.querySelector('#logout');
-logout.addEventListener('click',(evt)=>{
+logout.addEventListener('click', (evt) => {
     evt.preventDefault();
-    auth.signOut().then(()=>   {
+    auth.signOut().then(() => {
     });
 })
 
 const loginForm = document.querySelector('#login-form');
-loginForm.addEventListener('submit',(evt)=>{
+loginForm.addEventListener('submit', (evt) => {
     evt.preventDefault();
     //user info
     const email = loginForm['login-email'].value;
     const password = loginForm['login-password'].value;
-    auth.signInWithEmailAndPassword(email,password).then(cred=>{
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
         const modal = document.querySelector('#modal-login');
         M.Modal.getInstance(modal).close();
         loginForm.reset();
     })
 });
+
+const addForm = document.querySelector("#add-form");
+addForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    var user = firebase.auth().currentUser;
+        db.collection('books').add({
+            name: addForm['title'].value,
+            author: addForm['author'].value,
+            status: "false",
+            issueDate: "",
+            issuedBy: "",
+            phone: ""
+        }).then(() => {
+            const modal = document.querySelector('#modal-add');
+            M.Modal.getInstance(modal).close();
+            addForm.reset();
+        }).catch(error=>{
+            console.log('you are not admin');
+        });
+        const modal = document.querySelector('#modal-add');
+        M.Modal.getInstance(modal).close();
+        addForm.reset();
+})
+
+function send_verification() {
+    var user = firebase.auth().currentUser;
+    user.sendEmailVerification().then(function () {
+        window.alert('email verification sent');
+    }).catch(function (error) {
+        window.alert('error in sending email verificaion');
+    });
+}
--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,78 @@
 const bookList = document.querySelector('.books');
+const loggedOutLinks = document.querySelectorAll('.logged-out');
+const loggedInLinks = document.querySelectorAll('.logged-in');
+const about = document.getElementById("aboutData");
+
+const setupUI = (user) =>{
+    if(user){
+        loggedInLinks.forEach(item => {
+            item.style.display="block";
+            item.style.color ="grey";
+            item.style.fontWeight="bold";
+        });
+        loggedOutLinks.forEach(item => item.style.display="none");
+    }
+    else{
+        loggedInLinks.forEach(item => item.style.display="none");
+        loggedOutLinks.forEach(item => {
+            item.style.display="block";
+            item.style.color ="grey";
+            item.style.fontWeight="bold";
+        });
+    }
+}
+
 //setup books
 const setupBooks = (data) => {
     let html = '';
+    if(data.length){  
     data.forEach(doc =>{
         const book = doc.data();
         let cls='';
-        var st= book.status;
-        if(!st){
+        if(book.status==="false"){
             cls="available";
         }
         else{
             cls="issued"
         }
-        console.log(cls);     
-        console.log(st);
         const li = `
         <li>
-            <div class="collapsible-header grey-lighten-4 ${cls}">${book.name}</div>
+            <div class="collapsible-header grey-lighten-4 ${cls}">
+            <a href="#" class="btn-small btn-floating grey pulse">  
+            <i class="material-icons fav">favorite</i>
+            </a>
+                <div class="container">
+                <p>${book.name}</p>
+                <div class="subheader"> ~ ${book.author}</div>
+                </div>
+            </div>
             <div class="collapsible-body white">
-                <p>${book.author}</p>
+            <div class="container">            
                 <p>${book.issuedBy}</p>
                 <p>${book.issueDate}</p>
                 <p>${book.phone}</p>
-            </div>         
+            </div>    
+            </div>
         </li>`;
-        html += li; 
+        html += li;
     });
+    }
+    else{
+          html= `<li><div class="container center red-text" >
+          <h4>Register or Login to see the book list</h4>
+          </div></li>`
+    }
     bookList.innerHTML = html;
 }
+const setupAccount = (user)=>{
+    let html ='';
+    const userData = `
+    <p class="green-text show" > signed in as: ${user.email}</p>
+    `;
+    html+= userData;
+    about.innerHTML = html;
 
+}
 
 //setting materialize
 document.addEventListener('DOMContentLoaded',function(){
